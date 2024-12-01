4ce0ed5b24ab1123967f6615bb3c14b8e0a0a953
Marta en Mac <mvigara79@gmail.com>
2024-12-01 11:40:48 +0100

    feat(colors): Enhance fractal rendering and color interpolation
    - Improved color interpolation with Hermite smoothing
    - Added gamma correction for accurate color blending
    - Modified escape radius for better edge detail
    - Fixed color union handling
    - Optimized color calculations
    - Normalized color cycles
    - Code compliant with 42 Norm

--- src/colors/color.c
+++ src/colors/color.c
@@ -6,72 +6,72 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 18:13:31 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/01 11:19:47 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/01 11:36:54 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static uint32_t get_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+static uint32_t	get_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-   return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-static double smooth_color(double iter, double max_iter, double zx, double zy)
+static double	smooth_color(double iter, double max_iter, double zx, double zy)
 {
-    if (iter < max_iter)
-    {
-        // Logaritmo del módulo al cuadrado
-        double log_zn = log(zx * zx + zy * zy) / 2.0;
-        double nu = log(log_zn / log(2.0)) / log(2.0);
-        // Suavizado de la iteración
-        iter = iter + 1.0 - nu;
-    }
-    return iter;
+	double	log_zn;
+	double	nu;
+
+	if (iter < max_iter)
+	{
+		log_zn = log(zx * zx + zy * zy) / 2.0;
+		nu = log(log_zn / log(2.0)) / log(2.0);
+		iter = iter + 1.0 - nu;
+		iter += 0.5 * sin(iter * 0.1);
+	}
+	return (iter);
 }
 
-static t_color interpolate_colors(t_color c1, t_color c2, double t)
+static t_color	interpolate_colors(t_color c1, t_color c2, double t)
 {
-    t_color result;
-    // Interpolación con corrección gamma
-    double gamma = 2.2;
-    double t_corrected = pow(t, 1.0 / gamma);
+	t_color	result;
+	double	t_smooth;
+	double	gamma;
 
-    result.rgba.r = (uint8_t)(pow((1.0 - t_corrected) * pow(c1.rgba.r / 255.0, gamma) + 
-                    t_corrected * pow(c2.rgba.r / 255.0, gamma), gamma) * 255.0);
-    result.rgba.g = (uint8_t)(pow((1.0 - t_corrected) * pow(c1.rgba.g / 255.0, gamma) + 
-                    t_corrected * pow(c2.rgba.g / 255.0, gamma), gamma) * 255.0);
-    result.rgba.b = (uint8_t)(pow((1.0 - t_corrected) * pow(c1.rgba.b / 255.0, gamma) + 
-                    t_corrected * pow(c2.rgba.b / 255.0, gamma), gamma) * 255.0);
-    result.rgba.a = 255;
-    
-    return result;
+	t_smooth = t * t * (3 - 2 * t);
+	gamma = 2.2;
+	result.rgba.r = (uint8_t)(pow((1.0 - t_smooth) * pow(c1.rgba.r / 255.0,
+					gamma) + t_smooth * pow(c2.rgba.r / 255.0, gamma), gamma) * 255.0);
+	result.rgba.g = (uint8_t)(pow((1.0 - t_smooth) * pow(c1.rgba.g / 255.0,
+					gamma) + t_smooth * pow(c2.rgba.g / 255.0, gamma), gamma) * 255.0);
+	result.rgba.b = (uint8_t)(pow((1.0 - t_smooth) * pow(c1.rgba.b / 255.0,
+					gamma) + t_smooth * pow(c2.rgba.b / 255.0, gamma), gamma) * 255.0);
+	result.rgba.a = 255;
+	return (result);
 }
 
-void put_pixel_color(t_fractol *f)
+void	put_pixel_color(t_fractol *f)
 {
-    t_color     color1;
-    t_color     color2;
-    t_color     final_color;
-    double      smooth_iter;
-    uint32_t    rgba;
-    double      mod;
-    
-    mod = (f->z.re * f->z.re + f->z.im * f->z.im);
-    if (f->iter >= f->max_iter || mod <= 2.0)
-        mlx_put_pixel(f->img, f->x, f->y, 0xFFFFFFFF);
-    else
-    {
-        smooth_iter = smooth_color(f->iter, f->max_iter, f->z.re, f->z.im);
-        int index1 = ((int)smooth_iter) % f->palette->count;
-        int index2 = (index1 + 1) % f->palette->count;
-        double fract = smooth_iter - floor(smooth_iter);  
-        color1.value = f->palette->colors[index1];
-        color2.value = f->palette->colors[index2];  
-        final_color = interpolate_colors(color1, color2, fract);
-        rgba = get_color(final_color.rgba.r, final_color.rgba.g, 
-                        final_color.rgba.b, final_color.rgba.a);
-        
-        mlx_put_pixel(f->img, f->x, f->y, rgba);
-    }
+	t_color		final_color;
+	t_color		color1;
+	t_color		color2;
+	double		smooth_iter;
+	double		normalized_iter;
+
+	if (f->iter >= f->max_iter || (f->z.re * f->z.re + f->z.im * f->z.im) <= 4.0)
+		mlx_put_pixel(f->img, f->x, f->y, 0xFF000000);
+	else
+	{
+		smooth_iter = smooth_color(f->iter, f->max_iter, f->z.re, f->z.im);
+		normalized_iter = fmod(smooth_iter / 32.0, 1.0);
+		color1.value = f->palette->colors[(int)(normalized_iter
+				* (f->palette->count - 1))];
+		color2.value = f->palette->colors[((int)(normalized_iter
+				* (f->palette->count - 1)) + 1) % f->palette->count];
+		final_color = interpolate_colors(color1, color2, normalized_iter
+				* (f->palette->count - 1) - (int)(normalized_iter
+				* (f->palette->count - 1)));
+		mlx_put_pixel(f->img, f->x, f->y, get_color(final_color.rgba.r,
+				final_color.rgba.g, final_color.rgba.b, final_color.rgba.a));
+	}
 }
--- src/colors/palette.c
+++ src/colors/palette.c
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 12:03:04 by mvigara-          #+#    #+#             */
-/*   Updated: 2024/12/01 10:34:19 by mvigara-         ###   ########.fr       */
+/*   Updated: 2024/12/01 11:34:47 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,19 @@ t_palette *init_palettes(void)
     static t_palette array[6];
     
     array[0] = (t_palette){5, 0, {
-        0xFF1E1E5A, 0xFF4B0082, 0xFFDA70D6, 0xFFFFD700, 0xFF00CED1
+        0xFF1B1464, 0xFF0652DD, 0xFF9980FA, 0xFFFFD32A, 0xFFEA8685
     }};
     array[1] = (t_palette){5, 0, {
-        0xFF000033, 0xFF0066CC, 0xFFFF69B4, 0xFFFFD700, 0xFF87CEEB
+        0xFF000033, 0xFF4B0082, 0xFFDA70D6, 0xFFFFD700, 0xFF00CED1
     }};
     array[2] = (t_palette){5, 0, {
-        0xFF191970, 0xFF4169E1, 0xFFB8860B, 0xFFFF8C00, 0xFFE6E6FA
+        0xFF191970, 0xFF6A5ACD, 0xFF9370DB, 0xFFEE82EE, 0xFFFFA07A
     }};
-        array[3] = (t_palette){5, 0, {
+    array[3] = (t_palette){5, 0, {
         0xFF000080, 0xFF4B0082, 0xFF800080, 0xFFDB7093, 0xFFADD8E6
     }};
     array[4] = (t_palette){5, 0, {
-        0xFF191970, 0xFF6A5ACD, 0xFF9370DB, 0xFFEE82EE, 0xFFFFA07A
+        0xFF000066, 0xFF4169E1, 0xFFB8860B, 0xFFFF8C00, 0xFFE6E6FA
     }};
     array[5] = (t_palette){5, 0, {
         0xFF000033, 0xFF003366, 0xFF0099CC, 0xFF00CCFF, 0xFFFFFFFF
