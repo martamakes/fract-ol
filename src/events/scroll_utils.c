98000bd6cf772ca8c13db14e51138d7fa0c51b65
Marta en Mac <mvigara79@gmail.com>
2025-04-20 15:22:37 +0200

    scroll utils complies with norminette

--- src/events/scroll_utils.c
+++ src/events/scroll_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvigara- <mvigara-@student.42school.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 16:10:00 by mvigara-          #+#    #+#             */
-/*   Updated: 2025/04/21 16:10:00 by mvigara-         ###   ########.fr       */
+/*   Updated: 2025/04/20 15:21:47 by mvigara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,8 @@ void	calculate_complex_coords(t_fractol *f, int mx, int my,
 void	update_fractal_shift(t_fractol *f, int mx, int my,
 		t_complex mouse_pos)
 {
-	f->shift_x = mouse_pos.re - (mx - FRACT_SIZE / 2.0) 
-		* f->zoom / FRACT_SIZE;
-	f->shift_y = mouse_pos.im - (my - FRACT_SIZE / 2.0) 
-		* f->zoom / FRACT_SIZE;
+	f->shift_x = mouse_pos.re - (mx - FRACT_SIZE / 2.0) * f->zoom / FRACT_SIZE;
+	f->shift_y = mouse_pos.im - (my - FRACT_SIZE / 2.0) * f->zoom / FRACT_SIZE;
 }
 
 /* Check if mouse is in fractal image area */
